6dcfe9d9b31d073627895503036896418c8166be
vasilibrutsky <brutsky.vasily@gmail.com>
2018-09-04 16:36:56 +0300

    minor: added singlequotes and some more rules to basic eslint config (#34)

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ module.exports = {
   },
   extends: [
     'airbnb',
-    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:jsx-a11y/recommended',
   ],
+  rules: {
+    'max-len': ['error', { 'code': 120 }],
+    'react/forbid-prop-types': [1, { 'forbid': ['any'] }],
+    'react/jsx-sort-props': [1],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js'] }],
+    "react/jsx-one-expression-per-line": 0,
+  }
 };
